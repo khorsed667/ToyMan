6b7f125a3787714ee23c77e92ae1da047bb22636
khorsed667 <khorsedalam667@gmail.com>
2023-05-18 18:04:51 +0600

    primary setup completed

---
+++ src/Home/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Nav from '../../Shared/Nav';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../Shared/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='mx-24'>
+                <Nav></Nav>
+                <Outlet></Outlet>
+                <Footer></Footer>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Home/Home/Home';
+import Banner from './Home/Banner/Banner';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path:'/',
+        element:<Banner></Banner>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )

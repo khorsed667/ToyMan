4315e706c849ff3b610c87377f21ece60b678615
khorsed667 <khorsedalam667@gmail.com>
2023-05-18 20:08:16 +0600

    a few change inn gallery

--- src/Home/Gallery/Gallery.jsx
+++ src/Home/Gallery/Gallery.jsx
@@ -10,6 +10,9 @@ const Gallery = () => {
         "./../../../public/gallery-img/gal-3.jpg",
         "./../../../public/gallery-img/gal-4.jpg",
         "./../../../public/gallery-img/gal-5.jpg",
+        "./../../../public/gallery-img/gal-1.jpg",
+        "./../../../public/gallery-img/gal-2.jpg",
+        "./../../../public/gallery-img/gal-3.jpg",
       ];
     
 
@@ -22,15 +25,19 @@ const Gallery = () => {
 
         <section className="gallery-section py-10">
       <h2 className="text-3xl text-center font-bold mb-8">Explore Our Revved-Up Gallery</h2>
-      <p className="text-xl text-center mb-8">A Spectacular Showcase of Exciting Car Toys!</p>
+      <h2 className="text-xl text-center mb-8">A Spectacular Showcase of Exciting Car Toys</h2>
       <div className="flex flex-wrap">
         {carImages.map((imageUrl, index) => (
-          <div key={index} className="w-1/5 p-2 relative">
+          <div key={index} className="w-1/4 p-2 relative">
             <div className="gallery-item">
-              <img src={imageUrl} alt={`Car ${index + 1}`} className="w-full h-full object-cover" />
+              <img
+                src={imageUrl}
+                alt={`Car ${index + 1}`}
+                className="w-full h-full object-cover hover:scale-125"
+              />
               <div className="overlay">
                 <div className="text-center text-white absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
-                  <p className="text-xl font-bold">Explore your dream</p>
+                  <p className="text-xl font-bold">Explore your dream <br /> with Us</p>
                 </div>
               </div>
             </div>
@@ -38,29 +45,6 @@ const Gallery = () => {
         ))}
       </div>
     </section>
-
-
-
-
-
-
-
-
-
-
-
-        // <div>
-        //     <div className="text-center">this is  gallery serction</div>
-        //     <div className="flex w-full h-full items-center justify-center">
-        //         <div className="flex w-5/6 h-5/6">
-        //             <div className="slide relative flex-auto bg-cover bg-center transition-all duration-500 ease-in-out hover:flex-grow" style={{backgroundImage: `url('${backgroundeImgUrl1}')`}}></div>
-        //             <div className="slide relative flex-auto bg-cover bg-center transition-all duration-500 ease-in-out hover:flex-grow" style={{backgroundImage: `url('${backgroundeImgUrl2}')`}}></div>
-        //             <div className="slide relative flex-auto bg-cover bg-center transition-all duration-500 ease-in-out hover:flex-grow" style={{backgroundImage: `url('${backgroundeImgUrl3}')`}}></div>
-        //             <div className="slide relative flex-auto bg-cover bg-center transition-all duration-500 ease-in-out hover:flex-grow" style={{backgroundImage: `url('${backgroundeImgUrl4}')`}}></div>
-        //             <div className="slide relative flex-auto bg-cover bg-center transition-all duration-500 ease-in-out hover:flex-grow" style={{backgroundImage: `url('${backgroundeImgUrl5}')`}}></div>
-        //         </div>
-        //     </div>
-        // </div>
     );
 };
 

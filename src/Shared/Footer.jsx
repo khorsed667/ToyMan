de81dc4643ca8a61f59a62e3bf8f82a337667556
khorsed667 <khorsedalam667@gmail.com>
2023-05-23 23:44:31 +0600

    social media link added

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
       <footer className="footer p-10 bg-base-200 text-base-content">
         <div>
           <img src={logo} alt="" />
-          <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+          <p>CAR MAN Ltd.<br />Bashundhara; Dhaka; Bangladesh</p>
         </div>
         <div>
-          <span className="footer-title">Services</span>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <span className="footer-title">Contact Us with Social media</span>
+          <a className="link link-hover">Instagram</a>
+          <a className="link link-hover">Twitter</a>
+          <a className="link link-hover">Linkedin</a>
+          <a className="link link-hover">Facebook</a>
         </div>
         <div>
           <span className="footer-title">Company</span>
